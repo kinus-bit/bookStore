9574c65919db8639758ed52cf2ad6e62afeb344c
allan-bit-web <allankinuthia59@gmail.com>
2025-07-22 01:54:56 +0300

    updated import

--- client/src/components/form/BookForm.jsx
+++ client/src/components/form/BookForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Input } from '../ui/Input';
+import { Input } from '@/ui/Input';
 import { Label } from '../ui/Label';
 import { Textarea } from '../ui/Textarea';
 import { Button } from '../ui/Button';
